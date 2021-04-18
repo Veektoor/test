ac117baa8b64491cec8860784a08f972e91c5bc0
Veektoor <66330047+Veektoor@users.noreply.github.com>
2021-04-18 19:56:05 +0300

    Update test.py

--- test.py
+++ test.py
@@ -19,13 +19,13 @@ def addApp():
     for app in apps:
         label=tk.label(frame, text=app,bg="gray")
 
-canvas=tk.Canvas(root,height=700,width=700,bg="blue")
+canvas=tk.Canvas(root,height=500,width=500,bg="gray")
 canvas.pack()
 
-frame=tk.Frame(root,bg="black")
+frame=tk.Frame(root,bg="white")
 frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
 
-openFile=tk.Button(root,text="Open File",padx=10,pady=5,fg="white",
+openFile=tk.Button(root,text="Open File",padx=20,pady=8,fg="white",
                    bg="white", command=addApp)
 openFile.pack()
 runApps=tk.Button(root,text="Run Apps",padx=10,pady=5,fg="white",bg="white")
